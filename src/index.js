cc1c9dd31ef6443ecb72a336989dd003134ce66d
Alexander Hoinville <ahoin001@gmail.com>
2019-07-13 11:42:51 -0400

    Add background to canvas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-// Access the canvas in order to draw in it, will be using 2d context
-const canvas = document.getElementById('myCanvas');
-const ctx = canvas.getContext('2d');
+window.onload = function () {
 
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 150, 100);
+    // Access the canvas in order to draw in it, will be using 2d context
+    var canvas = document.getElementById('myCanvas');
+    var context = canvas.getContext('2d');
+
+    // create var holding an image for our background
+    var gameBackground = new Image();
+    gameBackground.src = "../img/city.jpg";
+
+    // If you try to call drawImage() before the image has finished loading, it won't do anything (or, in older browsers, may even throw an exception). So you need to be sure to use the load event so you don't try this before the image has loaded:
+    gameBackground.onload = function () {
+        context.drawImage(gameBackground, 0, 0, 900, 900);
+    };
+
+}
+
