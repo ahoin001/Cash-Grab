83ef5ffcfa71e4137505ebe55669a3580364da40
Alexander Hoinville <ahoin001@gmail.com>
2019-07-13 15:01:26 -0400

    Add movement into plate

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
 console.log(`canvas made`);
 
-// create plate 
+// create plate object
 var plate = new Plate(canvas, 400, 580);
 console.log(" - - -- - -", plate);
 
 // create var holding an image for our backeyPressedeyPressedground
 var gameBackground = new Image();
 gameBackground.src = "../img/city.jpg";
+
+    
 
 // If you try to call drawImage() before the image has finished loading, it won't do anything (or, in older browsers, may even throw an exception). So you need to be sure to use the load event so you don't try this before the image has loaded:
 // When image has loaded, imediaately draw it to the canvas
@@ -27,15 +29,23 @@ var drawEverything = function () {
     requestAnimationFrame(drawEverything);
 };
 
-// add listener for canvas to keep track of keys pressed
-canvas.addEventListener("keydown", function (e) {
-    var keyPressed = e.keyPressedeyCode;
-    if (keyPressed === 37 || keyPressed === 65) {
-        Plate.x -= 10;
-    }
-    else if (keyPressed === 39 || keyPressed === 68) {
-        Plate.x += 10;
-    }
-}, false);
+// when arrow key is pressed, listener catches it and takes action
+window.addEventListener("keydown", moveSomething, false);
+  
+function moveSomething(e) {
+    switch(e.keyCode) {
+        case 37:
+            // left key pressed
+            plate.x -= 20; 
+            
+            break;
+
+        case 39:
+            // right key pressed
+            plate.x += 20
+            break;
+
+    }   
+}   
 
 drawEverything();
