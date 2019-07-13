9399ad30a8e55809afb8a09e1ec0c7c232f6ba55
Alexander Hoinville <ahoin001@gmail.com>
2019-07-13 16:27:26 -0400

    Add template for falling objects

--- src/index.js
+++ src/index.js
@@ -2,6 +2,105 @@
 let canvas = document.getElementById('myCanvas');
 let context = canvas.getContext('2d');
 
+/****************************/
+
+// newly spawned objects start at Y=25
+var spawnLineY = 25;
+
+// spawn a new object every 1500ms
+var spawnRate = 1500;
+
+// set how fast the objects will fall
+var spawnRateOfDescent = 0.50;
+
+// when was the last object spawned
+var lastSpawn = -1;
+
+//  this array holds all spawned object
+var objects = [];
+
+//  save the starting time (used to calc elapsed time)
+var startTime = Date.now();
+
+// start animating
+// animate();
+
+const spawnRandomObject = () => {
+    // select a random type for this new object
+    var t;
+
+    // About Math.random()
+    // Math.random() generates a semi-random number
+    // between 0-1. So to randomly decide if the next object
+    // will be A or B, we say if the random# is 0-.49 we
+    // create A and if the random# is .50-1.00 we create B
+
+    if (Math.random() < 0.50) {
+        t = "red";
+    }
+    else {
+        t = "blue";
+    }
+
+    // create the new object
+    var object = {
+        // set this objects type
+        type: t,
+        // set x randomly but at least 15px off the canvas edges
+        //600 is my value canvas.width isnt working
+        x: Math.random() * (600 - 30) + 15,
+        // set y to start on the line where objects are spawned
+        y: spawnLineY,
+    }
+
+    // add the new object to the objects[] array
+    objects.push(object);
+}
+
+
+function animate(){
+
+    // get the elapsed time
+    var time=Date.now();
+
+    // see if its time to spawn a new object
+    if(time>(lastSpawn+spawnRate)){
+        lastSpawn=time;
+        spawnRandomObject();
+    }
+
+    // request another animation frame
+    //requestAnimationFrame(animate);
+
+    // clear the canvas so all objects can be 
+    // redrawn in new positions
+    //ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    // draw the line where new objects are spawned
+    context.beginPath();
+    context.moveTo(0,spawnLineY);
+    context.lineTo(canvas.width,spawnLineY);
+    context.stroke();
+
+    // move each object down the canvas
+    for(var i=0;i<objects.length;i++){
+        var object=objects[i];
+        object.y+=spawnRateOfDescent;
+        context.beginPath();
+        context.arc(object.x,object.y,8,0,Math.PI*2);
+        context.closePath();
+        context.fillStyle=object.type;
+        context.fill();
+    }
+
+}
+
+
+
+
+/***************************************************** */
+
+
 // create plate object
 let plate = new Plate(canvas, 400, 580);
 
@@ -24,10 +123,11 @@ const drawEverything = () => {
     // draws plate
     plate.render();
 
+    animate();
     // renders a new drop each time function is called
-    for (var i = 0; i < dropsArray.length; i++) {
-        dropsArray[i].render();
-    }
+    // for (var i = 0; i < dropsArray.length; i++) {
+    //     dropsArray[i].render();
+    // }
 
     // controlled infinite loop that keeps calling this function
     // in other words redraws background and plate
